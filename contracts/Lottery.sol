57b2cfbdc81ee833dc9b530a32559a9a4527aa32
hellovincenzo <vincenzo33.pellegrini@gmail.com>
2023-12-14 23:57:59 +0100

    refactoring enter function to addPlayer and refactoring random function, adding pickWinner function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,19 +4,29 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Lottery {
     address public manager;
-    address[] public players;
+    address payable[] public players;
 
     constructor() {
         manager = msg.sender;
-    }
-
-    function enter() public payable {
-        require(msg.value > .01 ether);
-        players.push(msg.sender);
     }
 
      function random() private view returns (uint) {
         return uint(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp, players)));
     }
 
+    function addPlayer(address payable newPlayer) public payable {
+        require(msg.value >= 0.01 ether, "Insufficient value sent");
+        players.push(newPlayer);
+    }
+
+    function pickWinner() public {
+        require(players.length > 0, "No players to pick a winner");
+
+        uint index = random() % players.length;
+        address payable winner = players[index];
+        winner.transfer(address(this).balance);
+        
+        // Optionally, reset the players array after picking a winner
+        delete players;
+    }
 }
